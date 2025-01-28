21a28e5bb47b38af46fe1794d5bbef5ca514294c
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2025-01-27 18:47:37 -0600

    Use cliffy for cmd line args parsing. Rework code & formatting of output.

--- main.ts
+++ main.ts
@@ -1,93 +1,189 @@
+import { Command } from "@cliffy/command";
 import plist from "https://esm.sh/simple-plist@1.4.0";
 
-if (import.meta.main) {
-  printData();
+class Runner {
+  constructor(readonly verbose: boolean, readonly output: string) {}
+
+  async run() {
+    const { output } = this;
+
+    const args = ["-ar", "-c", "AppleSmartBattery"];
+
+    this.logIfVerbose(`Running ioreg ${args.join(" ")}`);
+
+    const command = new Deno.Command("ioreg", {
+      args,
+    });
+
+    const { code, stdout } = await command.output();
+
+    if (code !== 0) {
+      console.log(`ioreg returned error code ${code}`);
+      Deno.exit(code);
+    }
+
+    const directory = `output/${output}`;
+    const filePath = `${Deno.cwd()}/${directory}/${output}`;
+
+    if (output) {
+      this.logIfVerbose(
+        `Writing ${stdout.length.toLocaleString()} bytes to ${filePath}.xml`
+      );
+
+      await Deno.mkdir(directory, { recursive: true });
+      await Deno.writeFile(`${filePath}.xml`, stdout);
+    }
+
+    this.logIfVerbose(
+      `Parsing ${stdout.length.toLocaleString()} bytes from ioreg stdout`
+    );
+
+    const data = plist.parse<AppleSmartBattery[]>(
+      new TextDecoder().decode(stdout)
+    );
+
+    if (output) {
+      const json = JSON.stringify(data, null, 2);
+
+      this.logIfVerbose(
+        `Writing ${json.length.toLocaleString()} bytes to ${filePath}.json`
+      );
+
+      await Deno.writeTextFile(`${filePath}.json`, json);
+    }
+
+    const root = data?.[0];
+    const adapterDetails = root?.AdapterDetails;
+
+    if (!root.ExternalConnected || !adapterDetails) {
+      console.log("No connected adapter details found, is it plugged in?");
+      Deno.exit(1);
+    }
+
+    const {
+      Manufacturer: manufacturer,
+      Name: name,
+      Description: description,
+      isWireless: isWireless,
+      UsbHvcMenu: capabilities,
+      UsbHvcHvcIndex: activeCapabilityIndex,
+    } = adapterDetails;
+
+    const details = [
+      { name: "Manufacturer", value: manufacturer },
+      { name: "Name", value: name },
+      { name: "Description", value: description },
+      { name: "Wireless", value: isWireless },
+    ];
+
+    if (adapterDetails.UsbHvcMenu?.length) {
+      details.push({
+        name: "Capabilities",
+        value: formatCapabilities(capabilities, activeCapabilityIndex),
+      });
+    }
+
+    const markdown = `Adapter Details:
+${details
+  .filter((detail) => !!detail.value)
+  .map((detail) => `* ${detail.name}: ${detail.value}`)
+  .join("\n")}`;
+
+    if (output) {
+      this.logIfVerbose(
+        `Writing ${markdown.length.toLocaleString()} bytes to ${filePath}.md`
+      );
+
+      await Deno.writeTextFile(`${filePath}.md`, markdown);
+    }
+
+    console.log(markdown);
+  }
+
+  // deno-lint-ignore no-explicit-any
+  logIfVerbose(...data: any[]) {
+    if (!this.verbose) {
+      return;
+    }
+
+    console.log(data);
+  }
 }
 
-async function printData() {
-  const args = Deno.args;
+await new Command()
+  .name("mac-adapter")
+  .description(
+    `Mac power adapter details utility.
 
-  const filename = args[0] ?? "output";
+If --output <name> is supplied, a directory will be created in the current working directory with the path: ./output/name/
 
-  console.log(`Running ioreg -acrw0 AppleSmartBattery`);
-  const command = new Deno.Command("ioreg", {
-    args: ["-afrw0", "-c", "AppleSmartBattery"],
-  });
+In this directory three files will be created:
 
-  const { code, stdout } = await command.output();
+* /output/name/name.xml
+  * the raw output of running the 'ioreg -acrw0 AppleSmartBattery' command, in PList XML format. This can be very hard to read.
 
-  if (code !== 0) {
-    console.log(`ioreg returned error code ${code}`);
-    return;
-  }
+* ./output/name/name.json
+  * the output converted to JSON format, which I find much easier to read, both manually and programmatically.
 
-  console.log(
-    `Writing ${stdout.length.toLocaleString()} bytes to ${filename}.xml`
-  );
+* ./output/name/name.md
+  * the adapter details from the output summarized in Markdown format.`
+  )
+  .version("1.0.0")
+  .option("-v, --verbose", "Print verbose debug information to the console.", {
+    default: false,
+  })
+  .option(
+    "-o, --output <name>",
+    "The name of optional output directory and files. If unspecified, no output directory or files will be created. See the Description above for more details.",
+    { default: "" }
+  )
+  .action(
+    async ({ verbose, output }) => await new Runner(verbose, output).run()
+  )
+  .parse();
 
-  await Deno.writeFile(`${filename}.xml`, stdout);
+function formatCapability(
+  { MaxVoltage, MaxCurrent, Index }: Capability,
+  activeIndex?: number
+): string {
+  if (!MaxCurrent || !MaxVoltage) return "";
 
-  console.log(`Parsing ${filename}.xml`);
-
-  const data = plist.readFileSync<AppleSmartBattery[]>(`${filename}.xml`);
-
-  const json = JSON.stringify(data, null, 2);
-
-  console.log(
-    `Writing ${json.length.toLocaleString()} bytes to ${filename}.json`
-  );
-
-  await Deno.writeTextFile(`${filename}.json`, json);
-
-  if (!data.length) {
-    console.log("No adapter details found, is it plugged in?");
-    return;
-  }
-
-  const adapterDetails = data[0].AdapterDetails;
-
-  if (!adapterDetails || adapterDetails.FamilyCode === 0) {
-    console.log("No adapter details found, is it plugged in?");
-    return;
-  }
-
-  const details = `Adapter Details:
-* Manufacturer: ${adapterDetails.Manufacturer}
-* Name: ${adapterDetails.Name}
-* Description: ${adapterDetails.Description}
-* Voltage: ${(adapterDetails.AdapterVoltage ?? 0) / 1000}V
-* Current: ${(adapterDetails.Current ?? 0) / 1000}A
-* Watts: ${adapterDetails.Watts}
-* Capabilities:
-${adapterDetails.UsbHvcMenu?.map(
-  (capability) =>
-    `  * ${capability.MaxVoltage / 1000}V ${capability.MaxCurrent / 1000}A`
-).join("\n")}`;
-
-  console.log(
-    `Writing ${details.length.toLocaleString()} bytes to ${filename}.md`
-  );
-
-  await Deno.writeTextFile(`${filename}.md`, details);
-
-  console.log(details);
+  return `${MaxVoltage / 1000}V ${MaxCurrent / 1000}A (${
+    (MaxVoltage / 1000) * (MaxCurrent / 1000)
+  }W)${activeIndex === Index ? " <- active" : ""}`;
 }
 
+function formatCapabilities(
+  capabilities?: Capability[],
+  activeIndex?: number
+): string | undefined {
+  if (!capabilities) return undefined;
+
+  return `
+  * ${capabilities
+    ?.map((item) => formatCapability(item, activeIndex))
+    .join("\n  * ")}`;
+}
 interface AppleSmartBattery {
   AdapterDetails: {
     AdapterVoltage?: number;
     Current?: number;
     Description?: string;
     FamilyCode: number;
+    isWireless?: boolean;
     Manufacturer?: string;
     Model?: string;
     Name?: string;
+    SerialString?: string;
     UsbHvcHvcIndex?: number;
-    UsbHvcMenu?: {
-      Index: number;
-      MaxCurrent: number;
-      MaxVoltage: number;
-    }[];
+    UsbHvcMenu?: Capability[];
     Watts?: number;
   };
+  ExternalConnected: boolean;
+}
+
+interface Capability {
+  Index: number;
+  MaxCurrent?: number;
+  MaxVoltage?: number;
 }
--- readme.md
+++ readme.md
@@ -2,34 +2,58 @@
 
 Use your MacBook as a USB-PD tester.
 
-Tested only on a MacBook Pro with M4 Pro chip, running MacOS Sequoia 15.1.1. It may run and work on other hardware, but you may need to run or compile using Deno.
+Tested only on a MacBook Pro with M4 Pro chip, running MacOS Sequoia 15.1.1. It may run and work on other hardware, but you may need to run or compile using [Deno](https://deno.com).
 
-Run `./mac-adapter output` where `output` is any file name.
+## Usage:
 
-It'll write three files:
+Run `./mac-adapter` from the command line.
 
-* output.xml
-* output.json
-* output.md
+Options:
 
-and print the adapter details out to the console.
+| Option | Description | Default |
+| ------ | ----------- | ------- |
+| -h, --help | Show this help. | false |
+| -V, --version | Show the version number for this program. | false |
+| -v, --verbose | Print verbose debug information to the console. | false |
+| -o, --output \<name> | The name of optional output directory and files. | "" |
 
-## Examples:
+If `--output <name>` is not specified, no output directory or files will be created.
 
-### Apple Power Adapter 1:
+If it is, a directory will be created in the current working directory with the path: ./output/name/
+
+In this directory three files will be created:
+
+* `/output/name/name.xml`
+  * the raw output of running the 'ioreg -acrw0 AppleSmartBattery' command, in PList XML format. This can be very hard to read.
+* `./output/name/name.json`
+  * the output converted to JSON format, which I find much easier to read, both manually and programmatically.
+* `./output/name/name.md`
+  * the adapter details from the output summarized in Markdown format.
+
+## Markdown Output Examples:
+
+### Apple 70W USB-C Power Adapter:
 
 Adapter Details:
-* Manufacturer: Apple
-* Name: 70W USB-C Power Adapter
+* Manufacturer: Apple Inc.
+* Name: 70W USB-C Power Adapter 
 * Description: pd charger
-* Voltage: 20V
-* Current: 3.39A
-* Watts: 68
+* Wireless: false
 * Capabilities:
-  * 5V 2.96A
-  * 9V 2.98A
-  * 15V 2.99A
-  * 30V 3.39A
+  * 5V 2.96A (14.8W)
+  * 9V 2.98A (26.82W)
+  * 15V 2.99A (44.85W)
+  * 20V 3.39A (67.8W) <- active
+
+### Apple 20W USB-C Power Adapter:
+
+Adapter Details:
+* Manufacturer: Apple Inc.
+* Name: 20W USB-C Power Adapter
+* Description: pd charger
+* Capabilities:
+  * 5V 3A (15W)
+  * 9V 2.22A (19.98W) <-active
 
 ### Battery pack 1:
 
@@ -37,24 +61,18 @@ Adapter Details:
 * Manufacturer: undefined
 * Name: undefined
 * Description: pd charger
-* Voltage: 5V
-* Current: 0.9A
-* Watts: 5
 * Capabilities:
-  * 5V 0.9A
+  * 5V 0.9A (4.5W) <-active
 
 ### Battery pack 2:
 
 Adapter Details:
 * Manufacturer: undefined
 * Name: undefined
-* Description: usb host
-* Voltage: 5V
-* Current: 3A
-* Watts: 15
+* Description: pd charger
 * Capabilities:
-  * 5V 3A
-  * 9V 2.22A
+  * 5V 3A (15W)
+  * 9V 2.22A (19.98W) <-active
 
 ### Battery pack 3:
 
@@ -66,6 +84,6 @@ Adapter Details:
 * Current: 1.5A
 * Watts: 18
 * Capabilities:
-  * 5V 2A
-  * 9V 2A
-  * 12V 1.5A
+  * 5V 2A (10W)
+  * 9V 2A (18W)
+  * 12V 1.5A (18W)
